913f095f291d80b7d7eb27d1e47bd8ea42d92a59
Daniel Clarke <deliotclarke@gmail.com>
2019-05-02 16:18:53 -0500

    added a click event to grab data from the form, then a new fetch POST call to post that, and a function to update the page with the new entry. it only took about 3 hours

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -5,9 +5,56 @@
     Change the fake variable names below to what they should be
     to get the data and display it.
 */
+
+//selectors for form inputs
+const date = document.querySelector("#date");
+const concept = document.querySelector("#workingConcept");
+const entry = document.querySelector("#jEntry");
+const mood = document.querySelector("#mood");
+const saveBtn = document.querySelector("#form_button");
+
+//populate DOM on load
 API.getJournalEntries().then(journalEntries => {
   journalEntries.forEach(entry => {
     const journalEntry = buildEntry.makeEntryComponent(entry)
     entriesToDOM.renderEntries(journalEntry)
   })
 });
+
+//entry grabber, builder and render-er
+function update() {
+  wrapper.innerHTML = "";
+  API.getJournalEntries().then(journalEntries => {
+    journalEntries.forEach(entry => {
+      const journalEntry = buildEntry.makeEntryComponent(entry)
+      entriesToDOM.renderEntries(journalEntry)
+    })
+  })
+}
+
+const entryFactory = (date, concept, entry, mood) => {
+  return {
+    "date": date,
+    "concept": concept,
+    "entry": entry,
+    "mood": mood
+  }
+}
+
+saveBtn.addEventListener("click", () => {
+  event.preventDefault()
+
+  //grab value of all the inputs
+  let newDate = date.value;
+  let newConcept = concept.value;
+  let newEntry = entry.value;
+  let newMood = mood.value;
+
+  //create a new object to send
+  const newJournalEntry = entryFactory(newDate, newConcept, newEntry, newMood);
+
+  //save the object to entries.json
+  API.saveJournalEntry(newJournalEntry).then(update)
+})
+
+// post.then(get).then(render)
